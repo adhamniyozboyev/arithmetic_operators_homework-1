e50f0742c8365cbe116c1a797f1f94292dc592a5
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-09-24 13:23:33 +0500

    main6

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -2,8 +2,9 @@
 number=87
 #Divide the number by 2 and assign number.
 number=number/2
-#Find the fraction of the number and assing to a variable called 'answer'.
+#Find the fraction of the number and assign to a variable called 'answer'.
 answer=number%1
 #Print the answer.
- 
+print(answer)
 #Print the number.
+print(number)
